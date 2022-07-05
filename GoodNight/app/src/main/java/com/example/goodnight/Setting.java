a54b90dce40fcd1823f81adbc94f71d59b82853f
SongJungHyun1004 <201802112@o.cnu.ac.kr>
2022-07-05 17:57:58 +0900

    setting view switching

--- GoodNight/app/src/main/java/com/example/goodnight/Setting.java
+++ GoodNight/app/src/main/java/com/example/goodnight/Setting.java
@@ -1,4 +1,24 @@
 package com.example.goodnight;
 
-public class Setting {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Setting extends AppCompatActivity {
+    Button b;
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.setting);
+
+        b = findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
 }
