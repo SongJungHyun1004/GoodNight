bdefd0525fcb2c49d185a2468931c8a0b239df9b
SongJungHyun1004 <201802112@o.cnu.ac.kr>
2022-07-06 19:49:37 +0900

    design setting view

--- GoodNight/app/src/main/java/com/example/goodnight/Setting.java
+++ GoodNight/app/src/main/java/com/example/goodnight/Setting.java
@@ -3,11 +3,16 @@ package com.example.goodnight;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TimePicker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Calendar;
+
 public class Setting extends AppCompatActivity {
     Button b;
+    TimePicker tp;
+    Calendar calendar;
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -20,5 +25,17 @@ public class Setting extends AppCompatActivity {
                 finish();
             }
         });
+
+        tp = (TimePicker) findViewById(R.id.timePicker);
+
+        calendar = Calendar.getInstance();
+        int hour, min;
+        if(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M){
+            hour = tp.getHour();
+            min = tp.getMinute();
+        }else{
+            hour = tp.getCurrentHour();
+            min = tp.getCurrentMinute();
+        }
     }
 }
